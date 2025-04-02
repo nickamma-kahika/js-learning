3c555c091e333b32de467e8b20fcc76e638963a7
Lakshay Vardhan <vardhanlakshay@gmail.com>
2025-04-02 17:16:53 +0530

    Router thing only

--- 04.1reactRouter/src/components/Header/Header.jsx
+++ 04.1reactRouter/src/components/Header/Header.jsx
@@ -5,14 +5,32 @@ const Header = () => {
   return (
     <div className= ' bg-gray-800 text-4xl text-white'>
       <ul className='flex flex-row m-5 p-5'>
-        <li className='m-5'>
-            <NavLink to='/' >App</NavLink>
+        <li className=' m-5 '>
+            <NavLink className={({ isActive }) =>
+              `px-4 py-2 rounded-2xl transition-all duration-300 ${
+                isActive
+                  ? 'bg-blue-500 text-white font-bold shadow-lg'
+                  : 'hover:bg-gray-700 hover:text-blue-300'
+              }`
+            } to='/' >App</NavLink>
         </li>
         <li className='m-5'>
-            <NavLink to='/Home'>Home</NavLink>
+            <NavLink className={({ isActive }) =>
+              `px-4 py-2 rounded-2xl transition-all duration-300 ${
+                isActive
+                  ? 'bg-blue-500 text-white font-bold shadow-lg'
+                  : 'hover:bg-gray-700 hover:text-blue-300'
+              }`
+            } to='/Home'>Home</NavLink>
         </li>
         <li className='m-5'>
-            <NavLink to='/deepika'>Deepika</NavLink>
+            <NavLink className={({ isActive }) =>
+              `px-4 py-2 rounded-2xl transition-all duration-300 ${
+                isActive
+                  ? 'bg-blue-500 text-white font-bold shadow-lg'
+                  : 'hover:bg-gray-700 hover:text-blue-300'
+              }`
+            } to='/deepika'>Deepika</NavLink>
         </li>
       </ul>
     </div>
